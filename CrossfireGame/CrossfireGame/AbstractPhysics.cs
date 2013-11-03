65812e0f89bbe282bf318299001ffbc5ba0b9491
emount <gentlemanoctopus@github.fake>
2013-11-03 17:38:24 +0000

    Recycling unused code from previous version

---
+++ CrossfireGame/CrossfireGame/AbstractPhysics.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Box2DX.Dynamics;
+
+namespace CrossFire
+{
+	/// <summary>
+	/// A facade layer between the model and the full Box2DX library, providing helper functions. 
+	/// </summary>
+	public class AbstractPhysics
+	{
+		/// <summary>
+		/// Produces an Enumerable for all of the bodies in a given World. 
+		/// </summary>
+		/// <param name="W"></param>
+		/// <returns></returns>
+		public static IEnumerable<Body> GetBodies(World W)
+		{
+			var b = W.GetBodyList();
+			do
+			{
+				yield return b;
+				b = b.GetNext();
+			}
+			while (b != null);
+
+		}
+	}
+}
---
+++ CrossfireGame/CrossfireGame/Model.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Box2DX;
+using Box2DX.Collision;
+using Box2DX.Common;
+using Box2DX.Dynamics;
+
+namespace CrossFire.MVC
+{
+	public class Model
+	{
+		World theWorld;
+		static Vec2 defaultGravity = new Vec2(0.0f, -9.8f);
+
+		public Model(float width, float height)
+			: this(width, height, defaultGravity)
+		{
+
+		}
+
+		/// <summary>
+		/// Create a new model
+		/// </summary>
+		/// <param name="width">Width in meters</param>
+		/// <param name="height">Height in meters</param>
+		/// <param name="gravity">Gravity vector. Defaults to (0, -9.8m/s²)</param>
+		public Model(float width, float height, Vec2 gravity)
+		{
+			AABB bounds = new AABB() { LowerBound = new Vec2(0, 0), UpperBound = new Vec2(width, height) };
+			theWorld = new World(bounds, gravity, true);
+		}
+
+		public Body CreateEntity(Vec2 pos, Vec2 vel )
+		{
+
+			BodyDef bodydef = new BodyDef();
+			bodydef.Position = pos;
+			bodydef.LinearVelocity = vel;
+			bodydef.MassData = new MassData(){ Mass = 1 };
+			var B = theWorld.CreateBody(bodydef);
+
+			PolygonShape P = new PolygonShape();
+			P.SetAsBox(0.05f, 0.05f);
+
+			FixtureDef fixtureDef = new PolygonDef() { Vertices = P.Vertices, VertexCount = P.VertexCount };
+			fixtureDef.Density = 1.0f;
+			fixtureDef.Friction = 0.3f;
+
+			B.CreateFixture(fixtureDef);
+			return B;
+
+		}
+
+		public IEnumerable<Body> GetBodies()
+		{
+			return AbstractPhysics.GetBodies(theWorld);
+		}
+
+
+		public void Compute(float seconds, int iterations, float dx=1/60.0f)
+		{
+			if (iterations <= 0) throw new ArgumentException();
+
+			for (int i = 0; i < seconds/dx; i++)
+			{
+				theWorld.Step(dx, iterations, iterations / 2);	
+			}
+		}
+
+	}
+}
