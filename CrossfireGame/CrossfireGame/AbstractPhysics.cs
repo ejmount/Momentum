ed210f450ffa54b5a92938763b3807773155fe56
emount <gentlemanoctopus@github.fake>
2013-11-03 18:28:04 +0000

    New method in abstract physics

--- CrossfireGame/CrossfireGame/AbstractPhysics.cs
+++ CrossfireGame/CrossfireGame/AbstractPhysics.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
 using Box2DX.Dynamics;
+using Box2DX.Common;
+using Box2DX.Collision;
 
 namespace CrossFire
 {
@@ -24,5 +26,28 @@ namespace CrossFire
 			while (b != null);
 
 		}
+
+
+		public Body CreateEntity(World W, float w, float h, Vec2 pos, Vec2 vel)
+		{
+			BodyDef bodydef = new BodyDef();
+			bodydef.Position = pos;
+			bodydef.LinearVelocity = vel;
+			bodydef.MassData = new MassData() { Mass = 1 };
+			var B = W.CreateBody(bodydef);
+
+			PolygonShape P = new PolygonShape();
+			P.SetAsBox(w, h);
+
+			FixtureDef fixtureDef = new PolygonDef() { Vertices = P.Vertices, VertexCount = P.VertexCount };
+			fixtureDef.Density = 1.0f;
+			fixtureDef.Friction = 0.3f;
+
+			B.CreateFixture(fixtureDef);
+			return B;
+
+		}
+
+
 	}
 }
