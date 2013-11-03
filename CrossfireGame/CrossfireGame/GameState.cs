71fa39d6cf6d5b0f20c420d427cecd45709c80d7
emount <gentlemanoctopus@github.fake>
2013-11-03 15:29:08 +0000

    Added XNA project, and GameState skeleton

---
+++ CrossfireGame/CrossfireGame/GameState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace CrossfireGame
+{
+	abstract class GameState
+	{
+		protected Game1 parent;
+
+		delegate void StateTransitionHandler(GameState dest);
+		event StateTransitionHandler OnTransition;
+
+		public GameState(Game1 parent)
+		{
+			this.parent = parent;
+		}
+
+		protected void Draw(GameTime time);
+		protected void Update(GameTime time);
+
+
+
+	}
+}
