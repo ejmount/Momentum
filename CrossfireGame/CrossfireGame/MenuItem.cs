6239d9421e47e831d78e1b0251f0cb8f251fae11
emount <gentlemanoctopus@github.fake>
2013-11-04 15:30:36 +0000

    Allows game states to be added to the menu without editing the menu

---
+++ CrossfireGame/CrossfireGame/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+sealed class MenuItem : Attribute
+{
+	public MenuItem(string Name)
+	{
+		this.Name = Name;
+	}
+
+	public string Name
+	{
+		get;
+		set; 
+	}
+
+	public int Order { get; set; }
+}
