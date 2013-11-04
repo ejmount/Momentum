24aeaaf137fd1589f5e6f52533bb793ac97d618f
emount <gentlemanoctopus@github.fake>
2013-11-04 20:52:16 +0000

    Working menu navigation, hurrah

--- CrossfireGame/CrossfireGame/RootMenu.cs
+++ CrossfireGame/CrossfireGame/RootMenu.cs
@@ -1,31 +1,135 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace CrossfireGame
 {
 	class RootMenu : GameState
 	{
+		List<Tuple<MenuItem, Type>> Options = new List<Tuple<MenuItem, Type>>();
+		int currentOption = 0;
+
 		public RootMenu(Game1 g)
 			: base(g) 
-		{ }
+		{
+			// ============================================================
+			// Get all of the types which have a MenuItem attribute, and then sort them into the correct order
+			// This is so the menu can be changed without editing any existing classes.
+			// Open-closed FTW!
+			// (Although perhaps excessively clever.)
+			// ============================================================
+			var MenuItemTypes = Assembly.GetExecutingAssembly().GetTypes()
+				.Where(T => T.IsSubclassOf(typeof(GameState)))
+				.Where(T => T.GetCustomAttributes(typeof(MenuItem),false).Length > 0);
+				
 
+			foreach (var Type in MenuItemTypes)
+			{
+				var attrib = (MenuItem)Type.GetCustomAttributes(typeof(MenuItem), false)[0];
+				var optionName = attrib.Name;
+				Options.Add(Tuple.Create(attrib, Type));
+			}
+
+			Options.Sort( (T1, T2) =>
+				{
+					var firstOrder = T1.Item1.Order;
+					var secondOrder = T2.Item1.Order;
+					return firstOrder.CompareTo(secondOrder);
+				});
+			// ============================================================
+			// ============================================================
+
+		}
+
+		const float MENU_HEIGHT = 100;
+		const float VERTICAL_MARGIN = 20;
+		SpriteFont MenuFont; 
 
 		public override void Draw(GameTime time)
 		{
-			parent.GraphicsDevice.Clear(Color.BurlyWood);
-			
+
+			if (MenuFont == null)
+				MenuFont = this.parent.Content.Load<SpriteFont>("defaultFont");
+			// Lazily initialized because I couldn't get the initialization ordering with LoadContent right. 
+
+			parent.GraphicsDevice.Clear(Color.DarkBlue);
+			SpriteBatch sb = new SpriteBatch(parent.GraphicsDevice);
+
+			float currentHeight = MENU_HEIGHT;
+
+			sb.Begin();
+			foreach(var curOptionindex in Options.Select( (T, i) => Tuple.Create(i, T)))
+			{
+				var curOption = curOptionindex.Item2;
+				var size = MenuFont.MeasureString(curOption.Item1.Name);
+				var pos = new Vector2(){ 
+					X = this.parent.GraphicsDevice.Viewport.Width/2 - size.X/2,
+					Y = currentHeight
+				};
+				var col = (curOptionindex.Item1 == currentOption) ? Color.White : Color.DarkGray;
+				sb.DrawString(MenuFont, curOption.Item1.Name, pos, col);
+				currentHeight += size.Y + VERTICAL_MARGIN;
+			}
+			sb.DrawString(MenuFont, currentOption.ToString(), Vector2.Zero, Color.Purple);
+
+			sb.End();
 		}
+
+		enum VerticalInput { Neutral, Up, Down }
+
+		VerticalInput prevInput = VerticalInput.Neutral;
+
+		private int Clamp(int v, int l, int u)
+		{
+			while (v >= u)
+				v -= (u - l);
+			while (v < l)
+				v += (u-l);
+			return v;
+		}
+
 
 		public override void Update(GameTime time)
 		{
-			if (GamePad.GetState(0).Buttons.B == ButtonState.Pressed)
+			VerticalInput curInput = VerticalInput.Neutral;
+
+			if (Keyboard.GetState(PlayerIndex.One).IsKeyDown(Keys.Up)
+				|| GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.DPadUp)
+				|| GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y > 0.5)
 			{
-				this.parent.ChangeState(typeof(GameMenu));
+				curInput = VerticalInput.Up;
 			}
+			else if (Keyboard.GetState(PlayerIndex.One).IsKeyDown(Keys.Down)
+				|| GamePad.GetState(PlayerIndex.One).IsButtonDown(Buttons.DPadDown)
+				|| GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y < -0.5)
+			{
+				curInput = VerticalInput.Down;
+			}
+
+			if (curInput != prevInput)
+			{
+				if (curInput == VerticalInput.Up)
+				{
+					currentOption = Clamp(currentOption - 1, 0, Options.Count);
+				}
+				else if (curInput == VerticalInput.Down)
+				{
+					currentOption = Clamp(currentOption + 1, 0, Options.Count);
+				}
+
+				prevInput = curInput;
+			}
+
+			if (GamePad.GetState(PlayerIndex.One).Buttons.A == ButtonState.Pressed
+				|| Keyboard.GetState(PlayerIndex.One).IsKeyDown(Keys.Enter))
+				this.parent.ChangeState(Options[currentOption].Item2);
+
+
 		}
 	}
 }
