43c8fa03521f6156983e0ac4516b4ab7460b577d
emount <gentlemanoctopus@github.fake>
2013-11-24 18:33:23 +0000

    Error-handling? Maybe?

--- CrossfireGame/CrossfireGame/Program.cs
+++ CrossfireGame/CrossfireGame/Program.cs
@@ -1,3 +1,5 @@
+using System.IO;
+using System.Linq;
 namespace CrossfireGame
 {
 #if WINDOWS || XBOX
@@ -9,10 +11,25 @@ namespace CrossfireGame
 		/// </summary>
 		private static void Main(string[] args)
 		{
+#if WINDOWS 
+			try {
+#endif
 			using (Game1 game = new Game1())
 			{
 				game.Run();
 			}
+#if WINDOWS
+			}
+			catch(System.Exception e)
+			{
+				var numFiles = new DirectoryInfo( System.Environment.CurrentDirectory).EnumerateDirectories().Count();
+				using (StreamWriter sw = new StreamWriter( "output" + numFiles.ToString("D4") + ".txt"))
+				{
+					sw.WriteLine(e.Message);
+					sw.WriteLine(e.StackTrace);
+				}
+			}
+#endif
 		}
 	}
 
